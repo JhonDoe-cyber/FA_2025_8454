645fd114e9bc0c7e3bc84c93ea160149db002221
Rhymes003 <n00466264@upn.pe>
2025-09-03 12:09:27 -0500

    subiendo ejercicio 1

---
+++ SEMANA1_c#/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA1_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+        static void ejer1()
+        {
+            String nombre, carrera;
+
+            Console.Write("Ingresar su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingresar su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido al curso de FA de la carrera {carrera}"); //declaramos variables
+        }
+        static void ejer2()
+        {
+         
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+
+    }
+}
